e792c695bf215249258a29d0f1576027e45b1ca0
vhs <90valentin@gmail.com>
2021-01-20 20:09:07 +0100

    styles: globals, tailwind classes as imports

---
+++ src/styles/stylesClasses.ts
@@ -0,0 +1,4 @@
+export const app =
+  "relative sm:w-screen md:p-20 lg:px-10 2xl:flex align-center space-between";
+export const h4 = "text-3xl underline pb-5";
+export const header = "flex content-center justify-between p-10"
