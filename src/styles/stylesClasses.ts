878f477f46ef283497f65ce2895472a3d6527ba2
vhs <90valentin@gmail.com>
2021-01-21 08:45:03 +0100

    styles: repocard, repocontainer, tabs

--- src/styles/stylesClasses.ts
+++ src/styles/stylesClasses.ts
@@ -1,4 +1,34 @@
 export const app =
-  "relative sm:w-screen md:p-20 lg:px-10 2xl:flex align-center space-between";
+`container shadow-sm relative align-center space-between
+sm:w-screen 
+md:p-20 
+lg:px-10`  
+
 export const h4 = "text-3xl underline pb-5";
-export const header = "flex content-center justify-between p-10"
+
+export const header = "flex content-center justify-between p-5"
+
+export const repoContainer =
+`p-3
+xl:grid xl:grid-cols-2 xl:p-0 xl:gap-x-5
+` 
+export const repoCard = "container shadow-md p-7 grid grid-cols-3 gap-2"
+
+export const repoName = "col-span-2 text-black"
+
+export const stargazers = "col-span-1 place-self-end"
+
+export const repoDescription = "col-span-3"
+
+export const tag = "text-sm bg-black rounded-full p-2 w-max"
+
+export const starButton = "col-span-2 place-self-end"
+
+export const nav = 
+`bg-black flex justify-around py-7
+ lg:bg-white lg:justify-start 
+`
+export const navItem =
+`place-self-center cursor-pointer hover:text-purple-800
+lg:place-self-end lg:bg-black lg:p-5 lg:m-5 lg:rounded-md
+`
